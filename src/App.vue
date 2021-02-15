a3b6b034c61b4cae2257d220248b17e4e1ff744b
Ajdin Imsirovic <ajdika@live.com>
2021-02-15 06:57:58 +0100

    Add form-input component from BootstrapVue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <HelloWorld msg="Todo all version 2"/>
+    <todo-input-field />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TodoInputField from './components/TodoInputField.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TodoInputField,
   }
 }
 </script>
---
+++ src/components/TodoInputField.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <b-form-input v-model="text" placeholder="Enter your name"></b-form-input>
+        <div class="mt-2">Value: {{ text }}</div>
+    </div>
+</template>
+
+<script>
+import { BFormInput } from "bootstrap-vue"
+
+export default {
+    name: 'TodoINputField',
+    components: [
+        BFormInput,
+    ],
+    data() {
+        return {
+            text: ''
+        }
+    }
+}
+</script>
